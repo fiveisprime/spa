614fced6ecf93abe980888963fa3a4130d0bfe22
Matt Hernandez <matt@modulus.io>
2014-01-04 22:05:37 -0500

    Removed the expres powered by message.

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('title', 'spa');
 
+app.disable('x-powered-by');
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -33,5 +35,5 @@ var controllers = require('./server/controllers')();
 require('./server/routes')(app, controllers);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server listening on port', app.get('port'));
 });
